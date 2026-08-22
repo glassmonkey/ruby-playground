ced3777cce8e13671f93cfd097b55ce04c548d03
永野峻輔 <peace.infi0407@gmail.com>
2026-08-22 21:37:39 +0900

    Add system tests for URL-based instant sharing
    Cover the round trip: editing code pushes a c param onto the URL,
    and visiting a URL with a c param restores that code into the editor.

--- test/system/playground_test.rb
+++ test/system/playground_test.rb
@@ -8,4 +8,23 @@ class PlaygroundTest < ApplicationSystemTestCase
     assert_selector "textarea#code"
     assert_selector "pre#output"
   end
+
+  test "editing the code updates the URL's c param without navigating away" do
+    visit root_path
+
+    fill_in "code", with: "1 + 1"
+
+    assert_current_path(/\?c=/, url: true)
+  end
+
+  test "loading a URL with a c param decodes it back into the editor" do
+    visit root_path
+    fill_in "code", with: "puts 'shared snippet'"
+    assert_current_path(/\?c=/, url: true)
+    shared_url = page.current_url
+
+    visit shared_url
+
+    assert_field "code", with: "puts 'shared snippet'"
+  end
 end
